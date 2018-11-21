86cffd47c0a139586928344002c04bcbefb4e3e0
dmr <formation@ilopes.me>
2018-11-21 16:05:34 +0100

    global

--- mainproject/src/app/app.component.ts
+++ mainproject/src/app/app.component.ts
@@ -6,11 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'form';
-
-  action(form) {
-    console.log(form);
-  }
-
-
+  title = 'mainproject';
 }
